4c1882825199033e348b26c3516f465f37b992c1
Fryskin <gman358325@gmail.com>
2023-07-30 14:08:41 +0200

    Content was added.

--- m_a_s/admin.py
+++ m_a_s/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from m_a_s.models import Newsletter, ClientOfService, Message, NewsletterLogs
+
+
+@admin.register(ClientOfService)
+class ClientOfServiceAdmin(admin.ModelAdmin):
+    list_display = ('last_first_middle_name', 'contact_email', 'comment',)
+    list_filter = ('last_first_middle_name',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('newsletter_status', 'time_of_sending_the_newsletter', 'interval',)
+    list_filter = ('newsletter_status', 'time_of_sending_the_newsletter',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_title', 'message_content',)
+    list_filter = ('message_title',)
+
+
+@admin.register(NewsletterLogs)
+class NewsletterLogsAdmin(admin.ModelAdmin):
+    list_display = ('datatime_of_last_try', 'status_of_try', 'response_of_mail_server',)
+    list_filter = ('status_of_try',)
--- m_a_s/apps.py
+++ m_a_s/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MASConfig(AppConfig):
+class MasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'm_a_s'
